25812d03fcbccb410097f437106234c87f3291e1
Surien <surien@suriens-imac.local>
2016-04-14 22:00:47 -0400

    Initial Commit

---
+++ Ray traceTests/Ray_traceTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ray_traceTests.swift
+//  Ray traceTests
+//
+//  Created by Surien on 2016-04-14.
+//  Copyright © 2016 Surien. All rights reserved.
+//
+
+import XCTest
+@testable import Ray_trace
+
+class Ray_traceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
